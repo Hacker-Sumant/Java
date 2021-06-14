ef6990edcb5fca1eda8b871caf18a601aad15b49
Sumant Kumar <78304397+Pawankryadav@users.noreply.github.com>
2021-06-13 19:00:54 -0600

    String chapter over

---
+++ Practiceset3.java
@@ -0,0 +1,19 @@
+public class Practiceset3 {
+    public static void main(String[] args) {
+        // question1
+        String name = "Sumant";
+        System.out.println(name);
+        System.out.println(name.toLowerCase());
+        // questin 2
+        String name1 = "Sum ant";
+        System.out.println(name1);
+        System.out.println(name1.replace(" ", "_"));
+        // questin 3
+        String letter = "Dear" + name + ", Thanks a lot";
+        System.out.println(letter);
+        // questin 4
+
+        // questin 5
+        System.out.println( "\"Dear " + name + "Thsi java is course is very nice \"" );
+    }
+}
--- string.java
+++ string.java
@@ -8,7 +8,7 @@ public class string {
         String upper = name.toUpperCase();
         String trim = name1.trim();
         String substring = name.substring(3);
-        String substring1 = name.substring(1,3);
+        String substring1 = name.substring(1, 3);
         String replace = name.replace('S', 'P');
         System.out.println("This is the length of sumant " + value);
         System.out.println("This is the length of sumant " + lower);
@@ -17,5 +17,23 @@ public class string {
         System.out.println("This is the substring of sumant " + substring);
         System.out.println("This is the substring of sumant " + substring1);
         System.out.println("This is the replace of sumant " + replace);
+        System.out.println("startsWith " + name.startsWith("Su"));
+        System.out.println("endsWith " + name.endsWith("Su"));
+        System.out.println("charAt " + name.charAt(5));
+        System.out.println("indexOf m " + name.indexOf("m"));
+        System.out.println("indexOf m " + name.indexOf("m", 1));
+        System.out.println("lastIndexOf m " + name.lastIndexOf("m"));
+        System.out.println("lastIndexOf m " + name.lastIndexOf("m", 4));
+        System.out.println("Equal to sumant  " + name.equals("harry"));
+        System.out.println("Equal to sumant  " + name.equals("Sumant"));
+        System.out.println("Equal to sumant  " + name.equalsIgnoreCase("SUMANT"));
+        System.out.println("I am escape sequence \" Sumant ");
+        System.out.println("I am escape sequence \\ Sumant ");
+        System.out.println("I am escape sequence \n Sumant ");
+        System.out.println("I am escape sequence \t Sumant ");
+        System.out.println("I am escape sequence \' Sumant ");
+        System.out.println("I am escape sequence \b Sumant ");
+        System.out.println("I am escape sequence \r Sumant ");
+        System.out.println("I am escape sequence \f Sumant ");
     }
 }
