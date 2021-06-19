188a6c5eee5ee6317c12feb1f486fa44e87ae376
Sumant Kumar <skhackerkr@gmail.com>
2021-06-18 23:01:33 -0600

    java contitional chapter

---
+++ Conditional.java
@@ -0,0 +1,10 @@
+public class Conditional {
+    public static void main(String[] args) {
+        int a = 19;
+        if(a>18){
+            System.out.println("You can drive");
+        }else{
+            System.out.println("You cannot drive");
+        }
+    }
+}
--- Practiceset3.java
+++ Practiceset3.java
@@ -9,11 +9,14 @@ public class Practiceset3 {
         System.out.println(name1);
         System.out.println(name1.replace(" ", "_"));
         // questin 3
-        String letter = "Dear" + name + ", Thanks a lot";
+        String letter = "Dear " + name + ", Thanks a lot";
         System.out.println(letter);
         // questin 4
-
+        String space = "This is  double space this is triple    space";
+        System.out.println(space.indexOf("  "));
+        System.out.println(space.indexOf("   "));
         // questin 5
-        System.out.println( "\"Dear " + name + "Thsi java is course is very nice \"" );
+         String a = "\"Dear "+ " name " + " This java is course is very nice \"";
+        System.out.println(a.replace("name", "SUMANT"));
     }
 }
